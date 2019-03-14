f6ac0d778f5d38adab66faa2c0c335b56e112069
nimi142 <rappaport.nimrod@gmail.com>
2019-03-14 20:32:34 +0200

    creating setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CubeEvasion',
+    version='v1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Nimrod Rappaport',
+    author_email='rappaport.nimrod@gmail.com',
+    description=''
+)
